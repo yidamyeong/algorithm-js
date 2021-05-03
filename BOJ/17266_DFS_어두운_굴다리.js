e5f9975e68fbaf9a9462a7173dd18ad39fbc1303
Yi, Damyeong | 이담영 <yidamyeong@gmail.com>
2021-05-03 17:57:35 +0900

    modify : changed Variables

--- BOJ/17266_DFS_어두운_굴다리.js
+++ BOJ/17266_DFS_어두운_굴다리.js
@@ -5,22 +5,22 @@ const input = '5\n2\n2 4';
 const NMX = input.split('\n');
 const N = parseInt(NMX[0]);
 const M = parseInt(NMX[1]);
-const XArr = NMX[2].split(' ').map(el => parseInt(el));
+const X = NMX[2].split(' ').map(el => parseInt(el));
 
-console.log(solution(N, M, XArr));
+console.log(solution(N, M, X));
 
-function solution(n, m, xArr) {
+function solution(n, m, x) {
     let h = 0;
     if (m === 1) {
-        return Math.max(xArr[0], (n - xArr[0]));
+        return Math.max(x[0], (n - x[0]));
     } else if (m > 1) {
-        for (let i = 0; i < xArr.length - 1; i++) {
+        for (let i = 0; i < x.length - 1; i++) {
             if (i === 0) {
-                h = Math.max((xArr[i]), (xArr[i + 1] - xArr[i]));
+                h = Math.max((x[i]), (x[i + 1] - x[i]));
             } else {
-                h = Math.max(h, (xArr[i + 1] - xArr[i]));
+                h = Math.max(h, (x[i + 1] - x[i]));
             }
         }
-        return Math.max(h, (n - xArr[m - 1]));
+        return Math.max(h, (n - x[m - 1]));
     }
 }
