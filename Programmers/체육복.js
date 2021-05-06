38848f80638e1a83447b0795e0afec08417c0b68
Yi, Damyeong | 이담영 <yidamyeong@gmail.com>
2021-05-06 22:32:36 +0900

    add : 코드 추가

--- Programmers/체육복.js
+++ Programmers/체육복.js
@@ -14,14 +14,21 @@ for (let i = 1; i <= n ; i++) {
     student.push(cnt);
 }
 console.log(student);
-for (let i = 1; i <= n; i++) {
+for (let i = 0; i < n; i++) {
     if (student[i] > 1) {
-        if (student[i - 1] < 1) {
-            student[i] -= 1;
-            student[i - 1] += 1;
-        } else if (student[i + 1] < 1) {
-            student[i] -= 1;
-            student[i - 1] += 1;
+        if (i > 0) {
+            if (student[i - 1] < 1) {
+                student[i] -= 1;
+                student[i - 1] += 1;
+            } else if (student[i + 1] < 1) {
+                student[i] -= 1;
+                student[i + 1] += 1;
+            }
+        } else {
+            if (student[i + 1] < 1) {
+                student[i] -= 1;
+                student[i + 1] += 1;
+            }
         }
     }
 }
@@ -34,3 +41,64 @@ for (let i of student) {
     }
 }
 console.log(result);
+
+// 또다른 풀이
+function solution(n, lost, reserve) {
+    const students = {};
+    let answer = 0;
+    for(let i = 1; i <= n; i++){
+        students[i] = 1;
+    }
+    lost.forEach(number => students[number] -= 1);
+    reserve.forEach(number => students[number] += 1);
+
+    for(let i = 1; i < n; i++){
+        if(students[i] === 2 && students[i-1] === 0){
+            students[i-1]++;
+            students[i]--;
+        } else if(students[i] === 2 && students[i+1] === 0){
+            students[i+1]++;
+            students[i]--;
+        }
+    }
+    for(let key in students){
+        if(students[key] >= 1){
+            answer++;
+        }
+    }
+    return answer;
+}
+
+
+// mino
+function solution2(n, lost, reserve) {
+    let students = Array(n).fill(1);
+    for (let r of reserve) {
+        students[r - 1] += 1;
+    }
+
+    for (let l of lost) {
+        students[l - 1] -= 1;
+    }
+
+    for (let i = 1; i < n; i++) {
+        let prev = students[i - 1];
+        let curr = students[i];
+        let next = students[i + 1];
+        if (curr > 1) {
+            if (prev == 0) {
+                students[i - 1] += 1;
+                students[i] -= 1;
+            } else if (next == 0) {
+                students[i + 1] += 1;
+                students[i] -= 1;
+            }
+        }
+    }
+
+    return students.filter(el => el > 0).length;
+}
+
+
+
+
